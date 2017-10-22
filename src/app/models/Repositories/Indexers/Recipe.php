01c0d7c4fda4fff23f1e7b88fa8e407e963a419e
DanmakuDan <DanmakuDan@gmail.com>
2017-10-21 21:11:24 -0500

    Add substitution logic LIST/using from JSON data, affects components, qualities, and tools. The substitutions can occur as a separate item group, or included in a group as a partial replacement. Item equivalent substitutions from the JSON sub tag also need to be expanded into the cache.

--- src/app/models/Repositories/Indexers/Recipe.php
+++ src/app/models/Repositories/Indexers/Recipe.php
@@ -22,6 +22,248 @@ class Recipe implements IndexerInterface
         $skills = [];
         foreach ($repo->raw(self::DEFAULT_INDEX) as $id) {
             $recipe = $repo->get(self::DEFAULT_INDEX.".".$id);
+
+            // handle group substitution of qualities, tools, and components
+            if (isset($recipe->using)) {
+                foreach ($recipe->using as $usinggroup) {
+                    $req = $repo->get("requirement.".$usinggroup[0]);
+                    if (isset($req)) {
+                        // the second value multiplies the values contained in the requirement
+                        $multiplier = $usinggroup[1];
+
+                        // apply component (crafting ingredient) substitutions
+                        if (isset($req->components)) {
+                            if (!isset($recipe->components)) {
+                                $recipe->components = array();
+                            }
+                            foreach ($req->components as $group_of_similar_components) {
+                                $new_component_groups=[];
+                                foreach ($group_of_similar_components as $similar_component_unit) {
+                                    list($id, $base_amount) = $similar_component_unit;
+                                    $mult_amount = $base_amount * $multiplier;
+                                    if ($mult_amount < 0) {
+                                        $mult_amount = -1;
+                                    }
+
+                                    $this->linkIndexes($repo, "toolFor", $id, $recipe);
+
+                                    if ($recipe->category == "uncraft"
+                                        or (isset($recipe->reversible)
+                                        and $recipe->reversible == "true")) {
+                                        $repo->append("item.disassembledFrom.$id", $recipe->repo_id);
+                                    }
+
+                                    $new_component_groups[] = array($id, $mult_amount);
+                                }
+                                if (count($new_component_groups)>0) {
+                                    $recipe->components[] = $new_component_groups;
+                                }
+                            }
+                        }
+
+                        // apply tool quality substitutions
+                        if (isset($req->qualities)) {
+                            if (!isset($recipe->qualities)) {
+                                $recipe->qualities = array();
+                            }
+
+                            foreach ($req->qualities as $quality_unit) {
+                                ValueUtil::SetDefault($quality_unit, "amount", 1);
+                                $recipe->qualities[] = $quality_unit;
+                            }
+                        }
+
+                        // apply tool usage (charge/quantity) substitutions
+                        if (isset($req->tools)) {
+                            if (!isset($recipe->tools)) {
+                                $recipe->tools = array();
+                            }
+
+                            foreach ($req->tools as $group_of_similar_tools) {
+                                $new_tool_group = array();
+                                foreach ($group_of_similar_tools as $similar_tool_unit) {
+                                    $new_tool_unit = [];
+                                    list($id, $base_amount) = $similar_tool_unit;
+                                    $mult_amount = $base_amount * $multiplier;
+                                    if ($mult_amount < 0) {
+                                        $mult_amount = -1;
+                                    }
+                                    $this->linkIndexes($repo, "toolFor", $id, $recipe);
+                                    $new_tool_group[] = array($id, $mult_amount);
+                                }
+                                if (count($new_tool_group)>0) {
+                                    $recipe->tools[] = $new_tool_group;
+                                }
+                            }
+                        }
+                    }
+                }
+            } // end "using" substitution
+
+            // handle component "LIST" substitution
+            if (isset($recipe->components)) {
+                foreach ($recipe->components as $cgkey => $similar_unit_group) {
+                    $splice_list = array();
+                    $newgrouplist = [];
+                    $insert_into_group=false;
+                    foreach ($similar_unit_group as $groupkey => $similar_unit) {
+                        unset($unit_param_3);
+                        list($u_id, $multiplier) = $similar_unit;
+                        if (count($similar_unit)>2) {
+                            $unit_param_3 = $similar_unit[2];
+                        }
+                        if (isset($unit_param_3) && $unit_param_3 == "LIST") {
+                            $req = $repo->get("requirement.".$u_id);
+                            if (isset($req) && isset($req->components)) {
+                                foreach ($req->components as $req_similar_unit_group) {
+                                    foreach ($req_similar_unit_group as $req_similar_unit) {
+                                        list($ru_id, $base_amount) = $req_similar_unit;
+                                        $mult_amount = $base_amount * $multiplier;
+                                        if ($mult_amount < 0) {
+                                            $mult_amount = -1;
+                                        }
+                                        $this->linkIndexes($repo, "toolFor", $ru_id, $recipe);
+
+                                        if ($recipe->category == "uncraft"
+                                            or (isset($recipe->reversible)
+                                            and $recipe->reversible == "true")) {
+                                            $repo->append("item.disassembledFrom.$ru_id", $recipe->repo_id);
+                                        }
+                                        $newgrouplist[] = array($ru_id, $mult_amount);
+                                    }
+                                }
+
+                                $splice_list[] = $groupkey;
+                            }
+                        } else {
+                            $insert_into_group=true;
+                        }
+                    }
+                    if (count($newgrouplist)>0) {
+                        if ($insert_into_group==true) {
+                            foreach ($newgrouplist as $groupitem) {
+                                $recipe->components[$cgkey][] = $groupitem;
+                            }
+                        } else {
+                            $recipe->components[] = $newgrouplist;
+                        }
+                    }
+
+                    if (count($splice_list)>0) {
+                        $splice_count = 0;
+                        foreach ($splice_list as $splice_number) {
+                            $splice_number -= $splice_count;
+                            array_splice($recipe->components[$cgkey], $splice_number, 1);
+                            $splice_count++;
+                        }
+                    }
+
+                    $splice_list2 = array();
+                    foreach ($recipe->components as $tool_groupkey => $tool_group) {
+                        if (count($tool_group)<1) {
+                            $splice_list2[]=$tool_groupkey;
+                        }
+                    }
+                    if (count($splice_list2)>0) {
+                        $splice_count = 0;
+                        foreach ($splice_list2 as $splice_number) {
+                            $splice_number -= $splice_count;
+                            array_splice($recipe->components, $splice_number, 1);
+                            $splice_count++;
+                        }
+                    }
+                }
+            } // end component "LIST" substitution
+
+            // handle tool "LIST" substitution
+            if (isset($recipe->tools)) {
+                foreach ($recipe->tools as $cgkey => $similar_unit_group) {
+                    $splice_list = array();
+                                $newgrouplist = [];
+                    $insert_into_group=false;
+                    foreach ($similar_unit_group as $groupkey => $similar_unit) {
+                        unset($unit_param_3);
+                        list($u_id, $multiplier) = $similar_unit;
+                        if (count($similar_unit)>2) {
+                            $unit_param_3 = $similar_unit[2];
+                        }
+                        if (isset($unit_param_3) && $unit_param_3 == "LIST") {
+                            $req = $repo->get("requirement.".$u_id);
+                            if (isset($req) && isset($req->tools)) {
+                                foreach ($req->tools as $req_similar_unit_group) {
+                                    foreach ($req_similar_unit_group as $req_similar_unit) {
+                                        list($ru_id, $base_amount) = $req_similar_unit;
+                                        $mult_amount = $base_amount * $multiplier;
+                                        if ($mult_amount < 0) {
+                                            $mult_amount = -1;
+                                        }
+
+                                        $this->linkIndexes($repo, "toolFor", $ru_id, $recipe);
+
+                                        $newgrouplist[] = array($ru_id, $mult_amount);
+                                    }
+                                }
+
+                                $splice_list[] = $groupkey;
+                            }
+                        } else {
+                            $insert_into_group=true;
+                        }
+                    }
+                    if (count($newgrouplist) > 0) {
+                        if ($insert_into_group == true) {
+                            foreach ($newgrouplist as $groupitem) {
+                                $recipe->tools[$cgkey][] = $groupitem;
+                            }
+                        } else {
+                            $recipe->tools[] = $newgrouplist;
+                        }
+                    }
+                    if (count($splice_list)>0) {
+                        $splice_count = 0;
+                        foreach ($splice_list as $splice_number) {
+                            $splice_number -= $splice_count;
+                            array_splice($recipe->tools[$cgkey], $splice_number, 1);
+                            $splice_count++;
+                        }
+                    }
+                    $splice_list2 = array();
+                    foreach ($recipe->tools as $tool_groupkey => $tool_group) {
+                        if (count($tool_group)<1) {
+                            $splice_list2[]=$tool_groupkey;
+                        }
+                    }
+                    if (count($splice_list2)>0) {
+                        $splice_count = 0;
+                        foreach ($splice_list2 as $splice_number) {
+                            $splice_number -= $splice_count;
+                            array_splice($recipe->tools, $splice_number, 1);
+                            $splice_count++;
+                        }
+                    }
+                }
+            } // end tool "LIST" substitution
+
+            if (isset($recipe->tools)) {
+                foreach ($recipe->tools as $toolgroupkey => $toolgroup) {
+                    $toolsub = array();
+                    foreach ($toolgroup as $toolunit) {
+                        $subitemlist = $repo->get_substitute($toolunit[0]);
+                        if (count($subitemlist)>0) {
+                            $toolsub[] = array($subitemlist, $toolunit[1]);
+                        }
+                    }
+                    if (count($toolsub)>0) {
+                        foreach ($toolsub as $toolsubgroup) {
+                            list($subgroup,$amount) = $toolsubgroup;
+                            foreach ($subgroup as $subitem) {
+                                $recipe->tools[$toolgroupkey][] = array($subitem, $amount);
+                            }
+                        }
+                    }
+                }
+            }
+
             // search for all the items with the apropiate qualities
             if (isset($recipe->qualities)) {
                 foreach ($recipe->qualities as $group) {
@@ -81,8 +323,10 @@ class Recipe implements IndexerInterface
             }
 
             // create a list of tools per category for this object.
-            $repo->append("item.toolForCategory.$id.$recipe->category",
-                $recipe->repo_id);
+            $repo->append(
+                "item.toolForCategory.$id.$recipe->category",
+                $recipe->repo_id
+            );
         }
 
         $repo->append("item.$key.$id", $recipe->repo_id);
@@ -97,7 +341,7 @@ class Recipe implements IndexerInterface
                 ValueUtil::SetDefault($recipe, "category", "uncraft");
             }
 
-            ValueUtil::SetDefault($recipe,"difficulty",0);
+            ValueUtil::SetDefault($recipe, "difficulty", 0);
 
             $repo->append(self::DEFAULT_INDEX, $recipe->repo_id);
             $repo->set(self::DEFAULT_INDEX.".".$recipe->repo_id, $recipe->repo_id);
@@ -113,7 +357,7 @@ class Recipe implements IndexerInterface
 
             if (isset($recipe->qualities)) {
                 foreach ($recipe->qualities as $group) {
-                    ValueUtil::SetDefault($group,"amount",1);
+                    ValueUtil::SetDefault($group, "amount", 1);
                 }
             }
 
@@ -129,12 +373,19 @@ class Recipe implements IndexerInterface
             if (isset($recipe->components)) {
                 foreach ($recipe->components as $group) {
                     foreach ($group as $component) {
+                        unset($listval);
                         list($id, $amount) = $component;
+                        if (count($component)>2) {
+                            $listval = $component[2];
+                        }
+                        if (isset($listval) && $listval == "LIST") {
+                            continue;
+                        }
                         $this->linkIndexes($repo, "toolFor", $id, $recipe);
 
                         if ($recipe->category == "uncraft"
-              or (isset($recipe->reversible)
-              and $recipe->reversible == "true")) {
+                            or (isset($recipe->reversible)
+                            and $recipe->reversible == "true")) {
                             $repo->append("item.disassembledFrom.$id", $recipe->repo_id);
                         }
                     }
