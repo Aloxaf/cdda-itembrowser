7e7841fa8db6e17d6e1253a1ef23f715ea74bddd
DanmakuDan <DanmakuDan@gmail.com>
2017-10-21 21:14:59 -0500

    Add requirement tag tracking to item browser.

---
+++ src/app/models/Repositories/Indexers/Requirement.php
@@ -0,0 +1,23 @@
+<?php
+namespace Repositories\Indexers;
+
+use Repositories\RepositoryWriterInterface;
+
+class Requirement implements IndexerInterface
+{
+    protected $database;
+
+    const DEFAULT_INDEX = "requirement";
+
+    public function onNewObject(RepositoryWriterInterface $repo, $object)
+    {
+        if ($object->type == "requirement") {
+            $repo->append(self::DEFAULT_INDEX, $object->id);
+            $repo->set(self::DEFAULT_INDEX.".".$object->id, $object->repo_id);
+        }
+    }
+
+    public function onFinishedLoading(RepositoryWriterInterface $repo)
+    {
+    }
+}
