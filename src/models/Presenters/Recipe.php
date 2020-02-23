7bb07c95fe197179e3a1fa9f9759e5e9d4b3333b
Aloxaf <aloxafx@gmail.com>
2020-02-23 11:56:20 +0800

    修复URL错误

--- src/models/Presenters/Recipe.php
+++ src/models/Presenters/Recipe.php
@@ -43,7 +43,7 @@ class Recipe extends \Robbo\Presenter\Presenter
             $inner = array();
             foreach ($group as $gi) {
                 list($item, $amount) = $gi;
-                $inner[] = '<a href="'.route("item.view", array("id" => $item->id))." ".($amount > 0 ? "($amount&nbsp;单位)" : "").'">'.$item->name.'</a>';
+                $inner[] = '<a href="'.route("item.view", array("id" => $item->id)).'">'.$item->name.($amount > 0 ? "(${amount}单位)" : "").'</a>';
             }
             $tools[] = implode(" 或 ", $inner);
         }
