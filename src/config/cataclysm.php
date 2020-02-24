c13e97eff8c7976ac81596d9fb93bea9789f7daf
Aloxaf <aloxafx@gmail.com>
2020-02-24 09:37:40 +0800

    更新相关链接

--- src/config/cataclysm.php
+++ src/config/cataclysm.php
@@ -2,8 +2,9 @@
 
 return array(
   "sites" => array(
-    "stable" => "localhost",
-    "development" => "localhost",
+    "英文稳定版(0.C)" => "cdda.chezzo.com",
+    "英文实验版(0.D+)" => "cdda-trunk.chezzo.com",
+    "中文实验版(0.D+)" => "cdda-trunk.aloxaf.cn",
   ),
 
   "searchCacheExpiration" => 60,
