694c644ba6a0ff000ed7859e21ef5a7bed701b8c
Aloxaf <aloxafx@gmail.com>
2020-04-08 10:57:41 +0800

    更新版本号

--- src/config/cataclysm.php
+++ src/config/cataclysm.php
@@ -2,9 +2,9 @@
 
 return array(
   "sites" => array(
-    "英文稳定版(0.C)" => "cdda.chezzo.com",
-    "英文实验版(0.D+)" => "cdda-trunk.chezzo.com",
-    "中文实验版(0.D+)" => "cdda-trunk.aloxaf.cn",
+    "英文稳定版(0.E)" => "cdda.chezzo.com",
+    "英文实验版(0.E+)" => "cdda-trunk.chezzo.com",
+    "中文实验版(0.E+)" => "cdda-trunk.aloxaf.cn",
   ),
 
   "searchCacheExpiration" => 60,
