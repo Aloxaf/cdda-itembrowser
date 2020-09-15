bd77953d69a84a32dfdb37ed9686d4a17510e632
Aloxaf <aloxafx@gmail.com>
2020-09-15 14:31:24 +0800

    更新错误页面

--- src/resources/views/errors/404.blade.php
+++ src/resources/views/errors/404.blade.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <title>服务器不可用</title>
+  <title>找不到该页面</title>
 
   <!-- Fonts -->
   <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -54,21 +54,10 @@
 <body>
   <div class="flex-center position-ref full-height">
     <div class="code">
-      503 </div>
+      404 </div>
 
-    <div class="message" style="padding: 10px; text-align: left;">
-      CDDA 物品浏览器当前不可用，因为我们正在进行维护。<br>
-      <br>
-      可能原因如下：<br>
-      <ul>
-        <li>更新导致需要重新编译数据库。</li>
-        <li>需要人工介入的复杂更新。</li>
-        <li>数据库自动升级。</li>
-      </ul>
-      请稍等几分钟后再尝试访问。
-      <p>如果长时间处于该状态（超过半个小时），请前往<a href="https://tieba.baidu.com/p/6905321647">此处</a>反馈情况。</p>
-
-    </div>
+    <div class="message" style="padding: 10px;">
+      当前页面对应的物品可能已经被移除 </div>
 
   </div>
 </body>
