4644fd21f10366df50ef688e6539555b298f0987
Aloxaf <aloxafx@gmail.com>
2021-05-19 10:39:43 +0800

    feat: 重新构建数据库后自动刷新搜索缓存

--- update.zsh
+++ update.zsh
@@ -3,7 +3,9 @@
 TRAPZERR() {
   cd $0:A:h
   LOG "Recovering..."
-  sudo -u www-data php -c ./php.ini src/artisan cataclysm:rebuild $dir.bak
+  mv $dir.bak $dir
+  php src/artisan down
+  sudo -u www-data php -c ./php.ini src/artisan cataclysm:rebuild $dir
   php src/artisan up
   exit
 }
@@ -16,7 +18,7 @@ cd $0:A:h
 
 LOG "Downloading latest source code..."
 rm -f 0.F-dev.zip
-curl -LOs https://github.wuyanzheshui.workers.dev/CleverRaven/Cataclysm-DDA/archive/refs/heads/0.F-dev.zip
+curl -LOs https://github.wuyanzheshui.workers.dev/CleverRaven/Cataclysm-DDA/archive/refs/heads/0.F-dev.zip || return
 
 LOG "Unzipping..."
 dir=Cataclysm-DDA-0.F-dev
@@ -34,19 +36,21 @@ bash ./lang/compile_mo.sh zh_CN
 python3 ../translate_json_strings.py
 popd
 
-LOG "Download latest Mods"
+LOG "Downloading latest Mods..."
 rm -f Kenan-Modpack-Mod.zip
 rm -rdf Kenan-Modpack-Chinese
 curl -LOs https://github.wuyanzheshui.workers.dev/linonetwo/CDDA-Kenan-Modpack-Chinese/releases/download/latest/Kenan-Modpack-Mod.zip
 unzip -qo Kenan-Modpack-Mod.zip
 cp -R Kenan-Modpack-Chinese/* $dir/data/mods
 
-LOG "Rebuilding database..."
+LOG "Building database..."
 php src/artisan down
 # sudo -u www-data php -c ./php.ini src/artisan cache:clear
 sudo -u www-data php -c ./php.ini src/artisan cataclysm:rebuild $dir
 php src/artisan up
 
+LOG "Building cache..."
+curl -sL "https://cdda-trunk.aloxaf.cn/search?q=丧尸浩克" > /dev/null || return
 # https://juejin.im/entry/5901af2e1b69e60058be2134
 
 # cp locale/zh_CN/LC_MESSAGES/cataclysm-dda.mo /usr/share/locale/zh_CN/LC_MESSAGES/
